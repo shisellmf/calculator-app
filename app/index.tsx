28f1c49c98e3f69f2e0e305b60c571bc0089fa59
Yenifer Shisell Morazan Ferrufino <ysm207@ADPensiones.com>
2025-08-14 12:23:13 -0600

    Adding methods: add,multiply,divide,substract and equals

--- app/index.tsx
+++ app/index.tsx
@@ -6,46 +6,58 @@ import { globalStyles } from "@/styles/global-style";
 import { View } from "react-native";
 
 export default function Calculator() {
-  const {formula,buildNumber} = useCalculator();
+  const {formula,prevNumber,
+        buildNumber,clean,toggleSign,deleteLast,
+        devideOperation,
+        multiplyOperation,
+        addOperation,
+        subtractOperation,
+        calculate,showResult} = useCalculator();
   
   return (
     <View style={globalStyles.calculatorContainer}>
       <View style={{paddingHorizontal:29,marginBlock:30}}>
         <ThemeText variant="h1">{formula}</ThemeText>
-        <ThemeText variant="h2">50</ThemeText>
+        {
+          formula===prevNumber?(
+            <ThemeText variant="h2"> </ThemeText>
+          ):(
+            <ThemeText variant="h2">{prevNumber}</ThemeText>
+          )
+        }
       </View>
       <View style={globalStyles.row}>
-        <CalculatorBotton onPress={()=>buildNumber('AC')} 
-          color={Colors.lightGray} blackText label='AC'></CalculatorBotton>
-        <CalculatorBotton onPress={()=>buildNumber('/')} 
+        <CalculatorBotton onPress={()=>clean()} 
+          color={Colors.lightGray} blackText label='C'></CalculatorBotton>
+        <CalculatorBotton onPress={()=>toggleSign()} 
           color={Colors.lightGray} blackText label='+/-'></CalculatorBotton>
-        <CalculatorBotton onPress={()=>buildNumber('%')} 
-          color={Colors.lightGray} blackText label='%'></CalculatorBotton>
-        <CalculatorBotton onPress={()=>buildNumber('/')} blackText 
+        <CalculatorBotton onPress={()=>deleteLast()} 
+          color={Colors.lightGray} blackText label='del'></CalculatorBotton>
+        <CalculatorBotton onPress={()=>devideOperation()} blackText 
           color={Colors.orange} label='/'></CalculatorBotton>
       </View>
       <View style={globalStyles.row}>
         <CalculatorBotton onPress={()=>buildNumber('7')} label='7'></CalculatorBotton>
         <CalculatorBotton onPress={()=>buildNumber('8')} label='8'></CalculatorBotton>
         <CalculatorBotton onPress={()=>buildNumber('9')} label='9'></CalculatorBotton>
-        <CalculatorBotton onPress={()=>buildNumber('x')} color={Colors.orange} label='X'></CalculatorBotton>
+        <CalculatorBotton onPress={()=>multiplyOperation()} color={Colors.orange} label='X'></CalculatorBotton>
       </View>
       <View style={globalStyles.row}>
         <CalculatorBotton onPress={()=>buildNumber('4')} label='4'></CalculatorBotton>
         <CalculatorBotton onPress={()=>buildNumber('5')} label='5'></CalculatorBotton>
         <CalculatorBotton onPress={()=>buildNumber('6')} label='6'></CalculatorBotton>
-        <CalculatorBotton onPress={()=>buildNumber('+')} color={Colors.orange} label='+'></CalculatorBotton>
+        <CalculatorBotton onPress={()=>addOperation()} color={Colors.orange} label='+'></CalculatorBotton>
       </View>
         <View style={globalStyles.row}>
         <CalculatorBotton onPress={()=>buildNumber('1')} label='1'></CalculatorBotton>
         <CalculatorBotton onPress={()=>buildNumber('2')} label='2'></CalculatorBotton>
         <CalculatorBotton onPress={()=>buildNumber('3')} label='3'></CalculatorBotton>
-        <CalculatorBotton onPress={()=>buildNumber('-')} color={Colors.orange} label='-'></CalculatorBotton>
+        <CalculatorBotton onPress={()=>subtractOperation()} color={Colors.orange} label='-'></CalculatorBotton>
       </View>
       <View style={globalStyles.row}>
         <CalculatorBotton onPress={()=>buildNumber('0')} doubleSize label='0'></CalculatorBotton>
         <CalculatorBotton onPress={()=>buildNumber('.')} label='.'></CalculatorBotton>
-        <CalculatorBotton onPress={()=>buildNumber('=')} color={Colors.orange} label='='></CalculatorBotton>
+        <CalculatorBotton onPress={()=>showResult()} color={Colors.orange} label='='></CalculatorBotton>
       </View>
     </View>
   );
--- hooks/useCalculator.tsx
+++ hooks/useCalculator.tsx
@@ -13,24 +13,47 @@ export const useCalculator = ()=>{
     const[number,setNumber]= useState('0');
     const[prevNumber,setPrevNumber]= useState('0');
 
-    const lastOperation = useRef<Operator | null>(null);
+    const lastOperation = useRef<Operator>(undefined);
 
-    const reset= ()=>{
-        setFormula('0');
+    const clean= ()=>{
         setNumber('0');
         setPrevNumber('0');
-        return setNumber('0');
+        setFormula('0');
+       
+        lastOperation.current= undefined;
     }
 
     useEffect(()=>{
-        //Todo:calcular
-        setFormula(number);        
-    },[number])
+        if(lastOperation.current){
+            const firstFormulaPart= formula.split(' ').at(0);
+            setFormula(`${firstFormulaPart} ${lastOperation.current} ${number}`)
+        }else{
+            setFormula(number)
+        }
+    },[number]);
+
+    useEffect(()=>{
+       const subResult= calculate();
+       setPrevNumber(subResult.toString())
+    },[formula]);
+
+    const toggleSign= ()=>{
+        if(number.includes('-')){
+            return setNumber(number.replace('-',''));
+        }
+        return setNumber('-'+number);        
+    }
+
+    const deleteLast=()=>{
+        if(number.length==1 || number.length==2 && number.includes('-')){
+            return setNumber('0');
+        }
+        
+        return setNumber(number.slice(0,-1));
+    }
 
     const buildNumber= (numberString:string)=>{
 
-        if(numberString.includes('AC'))return setNumber('0');
-        
         if(number.startsWith('0') && numberString=='0' 
         && !number.includes('.')){            
             return;
@@ -60,12 +83,81 @@ export const useCalculator = ()=>{
         setNumber(number+numberString);
     }
 
+    const setLastNumber= ()=>{
+        showResult();
+        if(number.endsWith('.')){
+            setPrevNumber(number.slice(0,-1))
+        }
+        setPrevNumber(number);
+        setNumber('0');
+    }
+
+    const devideOperation= ()=>{
+        setLastNumber();
+        lastOperation.current= Operator.divide;
+
+    }
+
+    const multiplyOperation= ()=>{
+        setLastNumber();
+        lastOperation.current= Operator.multiply;
+    }
+
+     const addOperation= ()=>{
+        setLastNumber();
+        lastOperation.current= Operator.add;
+    }
+
+     const subtractOperation= ()=>{
+        setLastNumber();
+        lastOperation.current= Operator.subtract;
+    }
+
+    const calculate=()=>{
+       
+        const [firstValue,Operation,secondValue] = formula.split(' ');
+        const num1= Number(firstValue)
+        const num2= Number(secondValue)
+        if(isNaN(num2))return num1;
+
+        switch (Operation){
+            case Operator.add:
+                return num1+num2;
+            case Operator.subtract:
+                return num1-num2;
+            case Operator.divide:
+                return num1 / num2;
+            case Operator.multiply:
+                return num1 * num2;
+            default:
+                throw new Error('Operación invalida')
+        }
+
+    }
+
+    const showResult=()=>{
+        const result = calculate();
+        setFormula(`${result}`);
+
+        lastOperation.current= undefined;
+        setPrevNumber('0');
+    }
+
     return{
         formula,
         number,
         prevNumber,
 
         //methods
-        buildNumber
+        buildNumber,
+        clean,
+        toggleSign,
+        deleteLast,
+        devideOperation,
+        multiplyOperation,
+        addOperation,
+        subtractOperation,
+        calculate,
+        showResult
     };
 }
